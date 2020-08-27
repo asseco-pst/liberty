ba7d9d00a49b8e88582f8849fb888b55f02e413c
PROMO\10000102 <helder.pestana@pst.asseco.com>
2020-08-27 15:02:05 +0100

    Added support for ignoring SSL verification

--- src/io/github/asseco/pst/liberty/services/AbstractDeployer.groovy
+++ src/io/github/asseco/pst/liberty/services/AbstractDeployer.groovy
@@ -2,6 +2,10 @@ package io.github.asseco.pst.liberty.services
 
 import io.github.asseco.pst.liberty.models.Profile
 
+import javax.net.ssl.*
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
+
 /**
  * Abstract class to setup default stuff
  *
@@ -12,7 +16,37 @@ abstract class AbstractDeployer implements IDeployerService {
     protected static String LIBERTY_DEPLOYMENT_PATH = '${server.output.dir}/dropins'
     protected final Profile profile
 
-    AbstractDeployer(Profile profile) {
+    AbstractDeployer(Profile profile, boolean acceptInsecureCertificates = false) {
         this.profile = profile
+
+        if (acceptInsecureCertificates) {
+            disableSSLVerification()
+        }
+    }
+
+    private static void disableSSLVerification() {
+        X509TrustManager nullTrustManager = new X509TrustManager() {
+            @Override
+            void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {}
+
+            @Override
+            void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {}
+
+            @Override
+            X509Certificate[] getAcceptedIssuers() {
+                return null
+            }
+        }
+
+        HostnameVerifier nullHostnameVerifier = new HostnameVerifier() {
+            @Override
+            boolean verify(String s, SSLSession sslSession) {
+                return true
+            }
+        }
+        SSLContext sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, [nullTrustManager] as TrustManager[], null)
+        HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory())
+        HttpsURLConnection.setDefaultHostnameVerifier(nullHostnameVerifier)
     }
 }
