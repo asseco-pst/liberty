b7d79dd3a3fe5990d516436e6548adde48f716ec
PROMO\10000102 <helder.pestana@pst.asseco.com>
2020-09-01 11:30:28 +0100

    Added new exceptions

---
+++ src/io/github/asseco/pst/liberty/exceptions/ArtifactUpdateException.groovy
@@ -0,0 +1,13 @@
+package io.github.asseco.pst.liberty.exceptions
+
+/**
+ * Classe used to throw UnsupportedClass exceptions.
+ *
+ * @date 29/07/2020
+ * @version 1.0.0
+ */
+class ArtifactUpdateException extends Exception {
+    ArtifactUpdateException(String message) {
+        super(message)
+    }
+}
