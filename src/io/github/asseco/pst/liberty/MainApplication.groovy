08cb1ebccfefa7062f6b544811b5a866ca62c051
Hélder Pestana <me@hgpestana.dev>
2020-09-01 17:42:49 +0100

    Revert "Feature/initial liberty packages"

---
+++ src/io/github/asseco/pst/liberty/MainApplication.groovy
@@ -0,0 +1,13 @@
+package io.github.asseco.pst.liberty
+
+/**
+ * Enter class description here
+ *
+ * @date 26/08/2020
+ * @version 1.0.0
+ */
+class MainApplication {
+    static void main(String[] args){
+        System.out.println("Hello world")
+    }
+}
